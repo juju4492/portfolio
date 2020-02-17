f26c266c1d139e3cd75b4ebe434c39f435d80f7d
Julien GREGOIRE <jgregoire9244@hotmail.fr>
2020-02-17 15:33:53 +0100

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
   get 'pages/home'
-  get 'pages/about'
-  get 'pages/project'
-  get 'pages/cv'
-  get 'pages/contact'
-  get 'home', to: 'pages#home', as: :home
-  get 'about', to: 'pages#about', as: :about
-  get 'project', to: 'pages#project', as: :project
-  get 'cv', to: 'pages#cv', as: :cv
-  get 'contact', to: 'pages#contact', as: :contact
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'home/index'
   root to: 'pages#home'
+  resources :home, only: [:home, :new, :create]
 end
