b18fbf95c8f5f33c12c3d2aea76bc6c2cc4b0222
Julien GREGOIRE <jgregoire9244@hotmail.fr>
2020-02-17 15:56:29 +0100

    .

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,13 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-// require jquery
+
 //= require rails-ujs
 //= require turbolinks
-require bootstrap-sprockets
+
 //= require_tree .
 //= require jquery_ujs
 //= require jquery.ui.all
 //= require turbolinks
+require jquery
+require bootstrap-sprockets
