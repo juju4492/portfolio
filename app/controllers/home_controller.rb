42e3e41958e325b0a91d8e6a66e35ba0a8b75380
juju4492 <jgregoire9244@hotmail.fr>
2020-05-07 23:40:38 +0200

    test page redirection après submit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,13 +12,12 @@ class HomeController < ApplicationController
       if @contact.deliver
         # re-initialize Home object for cleared form
         @contact = Home.new
-        format.html { render 'index'}
+        format.html { render 'pages/home'}
         format.js   { flash.now[:success] = @message = "Thank you for your message. I'll get back to you soon!" }
       else
-        format.html { render 'index' }
+        format.html { render 'pages/home' }
         format.js   { flash.now[:error] = @message = "Message did not send." }
       end
     end
-    redirect_to pages_home_path
   end
 end
