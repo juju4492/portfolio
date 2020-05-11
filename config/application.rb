cb22afe74478260452e0dcd9081ddf968afd2d65
juju4492 <jgregoire9244@hotmail.fr>
2020-05-11 23:02:22 +0200

    Mise en place des métadonnées

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
 require_relative 'boot'
-
+config.exceptions_app = self.routes
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'pages/home'
   get 'home/index'
   root to: 'pages#home'
-  # resources :home, only: [:index, :new, :create]
+  # match "/404", to: "errors#not_found", via: :all
+  # match "/422", to: "errors#unacceptable", via: :all
+  # match "/500", to: "errors#internal_server_error", via: :all
   post 'submit', to: 'home#create'
 end
 # fonctionne mais juste sur la page du form
