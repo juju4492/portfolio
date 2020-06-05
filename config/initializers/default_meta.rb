19ea0c2ef767e426da761c0171250d67b448d148
juju4492 <jgregoire9244@hotmail.fr>
2020-06-05 07:14:05 +0200

    Test sur les metadonnées en anglais

--- config/initializers/default_meta.rb
+++ config/initializers/default_meta.rb
@@ -1,2 +1,8 @@
 # Initialize default meta tags.
-DEFAULT_META = YAML.load_file(Rails.root.join("config/meta.yml"))
+if I18n.available_locales == "fr"
+DEFAULT_META = YAML.load_file(Rails.root.join("config/meta.fr.yml"))
+else
+  if I18n.available_locales == "en"
+DEFAULT_META = YAML.load_file(Rails.root.join("config/meta.en.yml"))
+end
+end
