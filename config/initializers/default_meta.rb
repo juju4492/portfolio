56a496cf5753bbfb63c1247b2ad041fffa10fa0f
juju4492 <jgregoire9244@hotmail.fr>
2020-06-05 07:19:50 +0200

    Rollback

--- config/initializers/default_meta.rb
+++ config/initializers/default_meta.rb
@@ -1,8 +1,2 @@
 # Initialize default meta tags.
-if I18n.available_locales == "fr"
-DEFAULT_META = YAML.load_file(Rails.root.join("config/meta.fr.yml"))
-else
-  if I18n.available_locales == "en"
-DEFAULT_META = YAML.load_file(Rails.root.join("config/meta.en.yml"))
-end
-end
+DEFAULT_META = YAML.load_file(Rails.root.join("config/meta.yml"))
